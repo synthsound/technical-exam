1255e529b57515a7e5f4d655e7c1a11185443bac
Thanakrit Kaewmun <yhoen01@gmail.com>
2020-07-08 15:31:22 +0700

    Initial Commit

---
+++ TechnicalTestTmp2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TechnicalTestTmp2
+//
+//  Created by SiN on 8/7/2563 BE.
+//  Copyright © 2563 SiN.Develop. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
